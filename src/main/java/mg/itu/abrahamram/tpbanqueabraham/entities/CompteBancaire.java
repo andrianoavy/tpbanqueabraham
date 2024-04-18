1bb83ed9f59f081ded7e524ac0a8b7bd6610c477
andrianoavy <abrahamramilison+andrianoavy@gmail.com>
2024-04-18 15:37:08 +0300

    Ajout de l'attribut @Version dans compte bancaire

--- src/main/java/mg/itu/abrahamram/tpbanqueabraham/entities/CompteBancaire.java
+++ src/main/java/mg/itu/abrahamram/tpbanqueabraham/entities/CompteBancaire.java
@@ -14,6 +14,7 @@ import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +38,9 @@ public class CompteBancaire implements Serializable {
 
 	private String nom;
 	private int solde;
+	@Version
+	private int version;
+
 	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
 	private List<OperationBancaire> operations = new ArrayList<>();
 
